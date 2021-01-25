af170b97e8f563c912f0ee73f961eb1ae271c690
Stavros Malakoudis <stavrina25@gmail.com>
2021-01-25 14:56:38 +0200

    finished patch creation

--- v0.c
+++ v0.c
@@ -23,7 +23,6 @@ double toc(struct timeval begin)
     return (stime);
 }
 
-
 /* Usage of the functions above 
 
         struct timeval tStart;
@@ -36,6 +35,8 @@ double toc(struct timeval begin)
 
 */
 
+double *denoiseImage(double *image, int size, int patchSize, double sigma);
+
 int main(int argc, char **argv[])
 {
     // int patchSize = 5;
@@ -44,27 +45,63 @@ int main(int argc, char **argv[])
     // for (int i = 0; i < patchSize; i++)
     //     printf("weight no %d is %f\n", i, weights[i]);
 
-
     //** CPU IMPLEMENTATION **//
-    int n;
+
+    int n; //n represents the dimentsions of a **SQUARE** image in pixels //
     char *file = (char *)malloc(20 * sizeof(char));
     char *name = (char *)malloc(20 * sizeof(char));
     name = "image3";
     sprintf(file, "%s.csv", name);
     int *image = readCSV(&n, file);
-    printf("size is %d\n",n);
+    printf("size is %d\n", n);
 
-    double *normal = normalizeImage(image, n); //each pixel has max value of 1
+    double *normal = normalizeImage(image, n); //each pixel has max value of 1 //
     char *normalName = (char *)malloc(20 * sizeof(char));
     sprintf(normalName, "%s_normal", name);
     writeToCSV(normal, n, normalName);
 
-    double *noisy = addNoiseToImage(normal, n); //add gaussian noise to the image
+    double *noisy = addNoiseToImage(normal, n); //add gaussian noise to the image//
     char *noisyName = (char *)malloc(20 * sizeof(char));
     sprintf(noisyName, "%s_noisy", name);
     writeToCSV(noisy, n, noisyName);
+}
 
+double *denoiseImage(double *image, int size, int patchSize, double sigma)
+{
+    //We assume that patchSize is an odd number//
+    //In order to create the patches we must consider that the pixels are stored in Row-Major format//
+    //A simple aproach is to handle the patches also in the same format//
+    int patchLimit = patchSize / 2;
+    int patchIterator, imageIterator;
 
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++) //go to each pixel of the image
+        {
+            int mod = j % size;
+            double *patch = (double *)malloc(patchSize * sizeof(double)); //We assume that (i,j) is the pixel on the centre
+            for (int k = -patchLimit; k <= patchLimit; k++)
+            {
+                for (int m = -patchLimit; m <= patchLimit; m++) //go to each pixel of the patch: i*size +j
+                {
+                    patchIterator = (k + patchLimit) * patchSize + (m + patchLimit);
+                    imageIterator = (i + k) * size + (j + m);
 
+                    if (imageIterator >= 0 && imageIterator < size * size) //filter out of image pixels
+                    {
 
+                        if (!(mod < patchLimit && m < -mod) && !(mod >= size - patchLimit && m >= size - mod))
+                            //!(j % size < patchLimit && m +  < 0) filters pixels that are on the left side of the patch
+                            //!(j % size >= size - patchLimit && m  >=size - j % size) filters pixels that are on the right side of the patch
+
+                            printf("patch: %d , image: %d, m is %d, size - mod is %d\n", patchIterator, imageIterator, m, size - mod);
+                    }
+
+                    // patch[patchIterator] = -1;
+                    // patch[patchIterator] = image [imageIterator];
+                }
+            }
+            printf("\n");
+        }
+    }
 }
