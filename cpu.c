393efa6f5af048ba500e274b5dd9637e2705dbaa
Stavros Malakoudis <stavrina25@gmail.com>
2021-02-03 17:18:01 +0200

    minor changes

--- cpu.c
+++ cpu.c
@@ -7,17 +7,32 @@
 #include <sys/time.h>
 
 
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
     //** CPU IMPLEMENTATION **//
-
-    int size; //size represents the dimentsions of a **SQUARE** image in pixels //
     char *file = (char *)malloc(20 * sizeof(char));
     char *name = (char *)malloc(20 * sizeof(char));
+
+    int patchSize;
+    float sigmaDist = 0.05; 
+    float sigmaGauss = 1.66;
+    if(argc==1){
+    patchSize = 7;
     name = "image3";
+    }
+    else if(argc==3){
+    patchSize=atoi(argv[1]);
+    name= argv[4];
+    }
+    else{
+        printf("Command line arguements are: PatchSize, name.\n");
+        exit;
+    }
+
+    int size; //size represents the dimentsions of a **SQUARE** image in pixels //
     sprintf(file, "%s.csv", name);
     int *image = readCSV(&size, file);
-    printf("Image dimensions are %dx%d\n", size,size);
+    printf("Image dimensions are %dx%d.\n", size,size);
 
     float *normal = normalizeImage(image, size); //each pixel has max value of 1 //
     char *normalName = (char *)malloc(20 * sizeof(char));
@@ -29,10 +44,6 @@ int main(int argc, char **argv[])
     sprintf(noisyName, "%s_noisy", name);
     writeToCSV(noisy, size, noisyName);
 
-    int patchSize = 7;
-    float sigmaDist = 0.05; 
-    float sigmaGauss = 1.66;
-
     struct timeval tStart;
 	tStart = tic();
 
@@ -41,7 +52,7 @@ int main(int argc, char **argv[])
     sprintf(denoisedName, "%s_denoised", name);
     writeToCSV(denoised, size, denoisedName);
 
-    printf("Cpu ook %.6f sec for %dx%d image with patch size = %d\n", toc(tStart),size,size,patchSize);
+    printf("%dx%d image with patch size = %d denoised with cpu in %.6f sec.\n",size, size, patchSize, toc(tStart));
 
     float *removed = findRemoved(noisy, denoised,size); //find difference from original//
     char *removedName = (char *)malloc(20 * sizeof(char));
